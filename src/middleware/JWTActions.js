e15743bd0dda47f986bafae985642563ffa1950d
ClowKhxu <clowkhuux2707@gmail.com>
2025-02-20 19:28:05 +0700

    cap nhat 1

--- src/middleware/JWTActions.js
+++ src/middleware/JWTActions.js
@@ -18,6 +18,8 @@ const verifyJWT = async (req, res, next) => {
     const accessToken = req.cookies?.access_token
     const refreshToken = req.cookies?.refresh_token
 
+    const decoded = JWTService.verifyToken(accessToken)
+
     if (!refreshToken) {
         return res.status(405).json({
             EC: -1,
@@ -25,47 +27,51 @@ const verifyJWT = async (req, res, next) => {
         })
     }
 
-    const decoded = JWTService.verifyToken(accessToken)
-
     if (accessToken && decoded !== 'TokenExpiredError') {
+
         req.user = {
             ...decoded,
             access_token: accessToken,
             refresh_token: refreshToken
         }
-        return next()
-    } 
 
-    const response = await JWTService.findUserByToken(refreshToken)
+        next()
+    } else {
+        const response = await JWTService.findUserByToken(refreshToken)
 
-    if (+response?.EC === -1) {
+        if (+response?.EC === -1) {
+            return res.status(401).json({
+                EC: response?.EC,
+                EM: response?.EM
+            })
+        }
+
+        const payload = {
+            id: response?.DT?.id,
+            username: response?.DT?.username,
+            email: response?.DT?.email,
+            gender: response?.DT?.gender,
+            phone_number: response?.DT?.phone_number,
+            address: response?.DT?.address,
+            type_account: response?.DT?.type_account,
+        }
+
+        const accessToken = JWTService.createJWT(payload)
+
+        req.user = {
+            ...payload,
+            access_token: accessToken,
+            refresh_token: refreshToken
+        }
+
+        // set cookies
+        handleInsertTokeToCookies(res, accessToken, refreshToken)
+
         return res.status(401).json({
-            EC: response?.EC,
-            EM: response?.EM
+            EC: -1,
+            EM: 'Phiên đăng nhập đã hết hạn!'
         })
     }
-
-    const payload = {
-        id: response?.DT?.id,
-        username: response?.DT?.username,
-        email: response?.DT?.email,
-        gender: response?.DT?.gender,
-        phone_number: response?.DT?.phone_number,
-        address: response?.DT?.address,
-        type_account: response?.DT?.type_account,
-    }
-
-    const newAccessToken = JWTService.createJWT(payload)
-
-    handleInsertTokeToCookies(res, newAccessToken, refreshToken)
-
-    req.user = {
-        ...payload,
-        access_token: newAccessToken,
-        refresh_token: refreshToken
-    }
-
-    return next()
 }
 
 module.exports = {
